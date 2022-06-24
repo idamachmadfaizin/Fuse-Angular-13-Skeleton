c1ad74e987fd4bc319b4067240f6eb5daef60817
Bishen Patel <patelzz007@gmail.com>
2022-06-25 02:55:38 +0800

    Updated package.json + added ng-select directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { LayoutModule } from 'app/layout/layout.module';
 import { AppComponent } from 'app/app.component';
 import { appRoutes } from 'app/app.routing';
 
+import { NgSelectModule } from '@ng-select/ng-select';
+
 const routerConfig: ExtraOptions = {
     preloadingStrategy       : PreloadAllModules,
     scrollPositionRestoration: 'enabled'
